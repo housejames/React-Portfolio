0b126733bdc832e72008c1f7cdf6872e63068032
housejames <housejames14@gmail.com>
2024-04-25 11:19:50 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import Header from './components/Home';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
+import Header from './components/Home.jsx';
+import About from './components/About.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Contact from './components/Contact.jsx';
+import Resume from './components/Resume.jsx';
+import Footer from './components/Footer.jsx';
 
 
 function App() {
